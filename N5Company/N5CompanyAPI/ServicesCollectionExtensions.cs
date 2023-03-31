e0c0a89ec3c4be89f65043c6b2dd9b3d6f3cddd9
Adam <tolosaadam@gmail.com>
2023-03-31 13:31:19 -0300

    Cleaning up startup.cs

---
+++ N5Company/N5CompanyAPI/ServicesCollectionExtensions.cs
@@ -0,0 +1,83 @@
+﻿using AutoMapper;
+using Microsoft.Extensions.DependencyInjection;
+using N5Company.Business.Interfaces;
+using N5Company.Business;
+using N5Company.Entities.Models;
+using N5Company.Repositories.Interfaces;
+using N5Company.Repositories.Repositories;
+using N5Company.Repositories;
+using N5Company.MapperProfiles;
+using N5Company.Commands.CreatePermission;
+using N5Company.Commands.UpdatePermission;
+using N5Company.Queries.GetPermissions;
+using System.Reflection;
+using Microsoft.Extensions.Configuration;
+using Nest;
+using System;
+using Microsoft.EntityFrameworkCore;
+
+namespace N5CompanyAPI
+{
+    public static class ServicesCollectionExtensions
+    {
+        public static IServiceCollection AddAndConfigureScopes(this IServiceCollection services)
+        {
+            services.AddScoped<IElasticSearchBusiness<Permission>, ElasticSearchBusiness<Permission>>();
+            services.AddScoped<IPermissionBusiness, PermissionBusiness>();
+            services.AddScoped<IPermissionRepository, PermissionRepository>();
+            services.AddScoped<IUnitOfWork, UnitOfWork>();
+            return services;
+        }
+
+        public static IServiceCollection AddAndConfigureMapperProfiles(this IServiceCollection services)
+        {
+            var config = new MapperConfiguration(cfg => {
+                cfg.AddProfile(new PermissionProfile());
+            });
+            IMapper mapper = config.CreateMapper();
+            services.AddSingleton(mapper);
+
+            return services;
+        }
+
+        public static IServiceCollection AddAndConfigureMediatR(this IServiceCollection services)
+        {
+            services.AddMediatR(cfg =>
+            {
+                cfg.RegisterServicesFromAssembly(typeof(CreatePermissionCommand).GetTypeInfo().Assembly);
+                cfg.RegisterServicesFromAssembly(typeof(GetAllPermissionsQuery).GetTypeInfo().Assembly);
+                cfg.RegisterServicesFromAssembly(typeof(UpdatePermissionCommand).GetTypeInfo().Assembly);
+            });
+
+            return services;
+        }
+
+        public static IServiceCollection AddAndConfigureElasticSearch(this IServiceCollection services, IConfiguration configuration)
+        {
+            var elasticsearchUrl = configuration.GetValue<string>("ElasticSearch:Url");
+            var elasticsearchIndex = configuration.GetValue<string>("ElasticSearch:Index");
+
+            var settings = new ConnectionSettings(new Uri(elasticsearchUrl))
+                .DefaultIndex(elasticsearchIndex);
+
+            var client = new ElasticClient(settings);
+
+            services.AddSingleton<IElasticClient>(client);
+
+            return services;
+        }
+
+        public static IServiceCollection AddAndConfigureDatabase(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<AppDbContext>(options =>
+            {
+                options.UseSqlServer(configuration.GetConnectionString("DefaultConnection"), x =>
+                {
+                    x.MigrationsAssembly("N5Company.Repositories");
+                });
+            });
+
+            return services;
+        }
+    }
+}
--- N5Company/N5CompanyAPI/Startup.cs
+++ N5Company/N5CompanyAPI/Startup.cs
@@ -1,23 +1,8 @@
-using AutoMapper;
-using System;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using N5Company.Business;
-using N5Company.Business.Interfaces;
-using N5Company.MapperProfiles;
-using N5Company.Repositories;
-using N5Company.Repositories.Interfaces;
-using N5Company.Repositories.Repositories;
-using System.Reflection;
-using N5Company.Queries.GetPermissions;
-using N5Company.Entities.Models;
-using N5Company.Commands.UpdatePermission;
-using Nest;
-using N5Company.Commands.CreatePermission;
 using Microsoft.OpenApi.Models;
 
 namespace N5CompanyAPI
@@ -34,27 +19,15 @@ namespace N5CompanyAPI
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            #region Database
-            services.AddDbContext<AppDbContext>(options =>
-            {
-                options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection"), x =>
-                {
-                    x.MigrationsAssembly("N5Company.Repositories");
-                });
-            });
-            #endregion
+            services.AddAndConfigureScopes();
 
-            #region Elastic Search
-            var elasticsearchUrl = Configuration.GetValue<string>("ElasticSearch:Url");
-            var elasticsearchIndex = Configuration.GetValue<string>("ElasticSearch:Index");
+            services.AddAndConfigureMapperProfiles();
 
-            var settings = new ConnectionSettings(new Uri(elasticsearchUrl))
-                .DefaultIndex(elasticsearchIndex);
+            services.AddAndConfigureMediatR();
 
-            var client = new ElasticClient(settings);
+            services.AddAndConfigureElasticSearch(Configuration);
 
-            services.AddSingleton<IElasticClient>(client);
-            #endregion
+            services.AddAndConfigureDatabase(Configuration);
 
             services.AddSwaggerGen(c =>
             {
@@ -62,28 +35,7 @@ namespace N5CompanyAPI
             });
 
             services.AddControllers();
-
-            services.AddScoped<IElasticSearchBusiness<Permission>, ElasticSearchBusiness<Permission>>();
-            services.AddScoped<IPermissionBusiness, PermissionBusiness>();
-            services.AddScoped<IPermissionRepository, PermissionRepository>();
-            services.AddScoped<IUnitOfWork, UnitOfWork>();
-
-            #region MediatR
-            services.AddMediatR(cfg =>
-            {
-                cfg.RegisterServicesFromAssembly(typeof(CreatePermissionCommand).GetTypeInfo().Assembly);
-                cfg.RegisterServicesFromAssembly(typeof(GetAllPermissionsQuery).GetTypeInfo().Assembly);
-                cfg.RegisterServicesFromAssembly(typeof(UpdatePermissionCommand).GetTypeInfo().Assembly);
-            });
-            #endregion
-
-            #region Auto Mapper
-            var config = new MapperConfiguration(cfg => {
-                cfg.AddProfile(new PermissionProfile());
-            });
-            IMapper mapper = config.CreateMapper();
-            services.AddSingleton(mapper);
-            #endregion
+           
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
